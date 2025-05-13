8fd50a9b1d2a207b106d999236dbbd53ad2983bf
Marina Borges Sarno <maborges@c3a10c1.42berlin.de>
2025-05-13 14:10:05 +0200

    linked assets path

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,16 +6,24 @@
 /*   By: maborges <maborges@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 22:48:45 by maborges          #+#    #+#             */
-/*   Updated: 2025/05/12 18:20:38 by maborges         ###   ########.fr       */
+/*   Updated: 2025/05/13 14:04:12 by maborges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef SO_LONG_H
 # define SO_LONG_H
 
+// DIsplay size definitions
 #define WIDTH 800
 #define HEIGHT 600
 #define TILE 32
+
+// Textures path definitions
+# define PATH_CAT "../assets/cat.xpm"
+# define PATH_CHICKEN "../assets/chicken.xpm"
+# define PATH_FARMTILE "../assets/farmTile.xpm"
+# define PATHGRASS "../assets/grass.xpm"
+# define PATH_HOUSE "../assets/house.xpm"
 
 #include <mlx.h> // include minilibX header
 #include <stdlib.h> // include standard library header
